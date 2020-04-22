74da3ecd2937b99df8402f69ad9536452dd47a87
xiaoza7 <1599125343@qq.com>
2020-04-22 10:21:26 +0800

    add the reresponse enetity

---
+++ src/main/java/com/lmqrpc/entity/ReResponse.java
@@ -0,0 +1,42 @@
+package com.lmqrpc.entity;
+
+import java.io.Serializable;
+
+public class ReResponse  implements Serializable {
+
+
+
+    private String uniqueId;
+
+    public String getUniqueId() {
+        return uniqueId;
+    }
+
+    public void setUniqueId(String uniqueId) {
+        this.uniqueId = uniqueId;
+    }
+
+    public Object getResult() {
+        return result;
+    }
+
+    public void setResult(Object result) {
+        this.result = result;
+    }
+
+    public int getTimeout() {
+        return timeout;
+    }
+
+    public void setTimeout(int timeout) {
+        this.timeout = timeout;
+    }
+
+    private Object result;
+
+    private int timeout;
+
+
+
+
+}
---
+++ src/main/java/com/lmqrpc/nettyserver/NettyServerFactory.java
@@ -0,0 +1,4 @@
+package com.lmqrpc.nettyserver;
+
+public class NettyServerFactory {
+}
