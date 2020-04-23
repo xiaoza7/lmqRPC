47ff25ede8f7970cfa1cad563b78e7d123453164
xiaoza7 <1599125343@qq.com>
2020-04-23 16:36:25 +0800

    update the nettyprovider ,add the stop

--- src/main/java/com/lmqrpc/nettyserver/NettyServerFactory.java
+++ src/main/java/com/lmqrpc/nettyserver/NettyServerFactory.java
@@ -2,6 +2,7 @@ package com.lmqrpc.nettyserver;
 
 import com.lmqrpc.entity.RcRequest;
 import com.lmqrpc.seriable.ReNettyDecoder;
+import com.lmqrpc.seriable.ReNettyEncoder;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelInitializer;
@@ -12,9 +13,18 @@ import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * lmq
+ */
+//这里使用netty的reactor代替原生的threadpool+动态代理
 
 public class NettyServerFactory {
 
+
+    private static final Logger log= LoggerFactory.getLogger(NettyServerFactory.class);
     private static NettyServerFactory  nettyServer=new NettyServerFactory();
     private Channel channel;
 
@@ -22,31 +32,64 @@ public class NettyServerFactory {
     private EventLoopGroup bossGroup;
     private EventLoopGroup workerGroup;
 
-    public void startServer(int port)
-    {
-        synchronized (NettyServerFactory.class){
+    public void startServer(int port) {
+        synchronized (NettyServerFactory.class) {
 
-            if(bossGroup!=null||workerGroup!=null)
-            {
-                return ;
+            if (bossGroup != null || workerGroup != null) {
+                return;
             }
 
             bossGroup = new NioEventLoopGroup();
-            workerGroup=new NioEventLoopGroup();
-            ServerBootstrap serverBootstrap=new ServerBootstrap();
-            serverBootstrap.group(bossGroup,workerGroup).channel(NioServerSocketChannel.class).option(ChannelOption.SO_BACKLOG,1024).childOption(ChannelOption.SO_KEEPALIVE,true)
-                    .childOption(ChannelOption.TCP_NODELAY,true).handler(new LoggingHandler(LogLevel.INFO)).childHandler(new ChannelInitializer<SocketChannel>() {
+            workerGroup = new NioEventLoopGroup();
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class).option(ChannelOption.SO_BACKLOG, 1024).childOption(ChannelOption.SO_KEEPALIVE, true)
+                    .childOption(ChannelOption.TCP_NODELAY, true).handler(new LoggingHandler(LogLevel.INFO)).childHandler(new ChannelInitializer<SocketChannel>() {
 
                 protected void initChannel(SocketChannel socketChannel) throws Exception {
                     socketChannel.pipeline().addLast(new ReNettyDecoder(RcRequest.class));
+                    socketChannel.pipeline().addLast(new ReNettyEncoder());
+                    socketChannel.pipeline().addLast(new NettyProviderHandler());
 
 
                 }
             });
 
+            try {
+                channel = serverBootstrap.bind(port).sync().channel();
+
+                log.info(" the netty server is started!");
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
         }
 
 
+
+
+    }
+
+
+    public void  stopServer()
+    {
+        if (null == channel) {
+            throw new RuntimeException("Server is Stoped");
+        }
+        bossGroup.shutdownGracefully();//关闭
+        workerGroup.shutdownGracefully();
+        channel.closeFuture().syncUninterruptibly();
+        log.info(" the netty server is stoped!");
+
+
+    }
+
+
+    private NettyServerFactory() {
+    }
+
+
+    public static NettyServerFactory singleton() {
+        return nettyServer;
     }
 
 
