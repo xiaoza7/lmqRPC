865040eae57f674169c0da6b10659af0c2839bf0
xiaoza7 <1599125343@qq.com>
2020-04-27 13:52:45 +0800

    add the consumer callable

---
+++ src/main/java/com/lmqrpc/entity/InvokerServiceCallable.java
@@ -0,0 +1,9 @@
+package com.lmqrpc.entity;
+
+import java.util.concurrent.Callable;
+
+public class InvokerServiceCallable implements Callable<RcResponse> {
+    public RcResponse call() throws Exception {
+        return null;
+    }
+}
